776068f2f53121e7a7debcb79ced111a378cada4
Smooth Operator <github@andy.disruptek.com>
2026-02-05 09:07:18 -0500

    breakdown

--- tests/integration/mod.rs
+++ tests/integration/mod.rs
@@ -5,8 +5,17 @@ mod core_basic {
 mod advanced_features {
     include!("advanced_features.rs");
 }
-mod phase_tests {
-    include!("phase_tests.rs");
+mod phase4_ecosystem {
+    include!("phase4_ecosystem.rs");
+}
+mod phase3_performance {
+    include!("phase3_performance.rs");
+}
+mod phase2_advanced {
+    include!("phase2_advanced.rs");
+}
+mod phase1_stability {
+    include!("phase1_stability.rs");
 }
 mod macros {
     include!("macros.rs");
