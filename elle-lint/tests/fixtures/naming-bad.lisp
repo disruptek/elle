624a50a2b8b3484e26324a1217b8bb375a41fdac
Smooth Operator <github@andy.disruptek.com>
2026-02-26 12:33:36 -0500

    Step 10: Delete old crate directories
    - Delete elle-lint/ directory
    - Delete elle-lsp/ directory
    - All tests pass

--- elle-lint/tests/fixtures/naming-bad.lisp
+++
@@ -1,6 +0,0 @@
-; Bad naming conventions - should be flagged by linter
-
-(var myVariable 10)
-(var camelCase 42)
-(var PascalCase 100)
-(var snake_case 5)
--- elle-lint/tests/fixtures/naming-good.lisp
+++
@@ -1,8 +0,0 @@
-; Good naming conventions
-
-(var square 42)
-(var my-variable 10)
-(def add-two (fn (x y) (+ x y)))
-(def number? (fn (x) (int? x)))
-(def set-value! (fn (x v) v))
-(var foo-bar-baz 123)
