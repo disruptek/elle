bfed924d91a23b539cb36527cf01655e0dd9b53c
Smooth Operator <github@andy.disruptek.com>
2026-03-09 12:10:51 -0400

    refactor: remove old test-modules directory

--- test-modules/test.lisp
+++
@@ -1,7 +0,0 @@
-# Test module for import-file integration tests
-(def test-var 42)
-(def test-string "hello")
-(var test-list (list 1 2 3))
-
-# Module exports
-(fn [] {:test-var test-var :test-string test-string :test-list test-list})
